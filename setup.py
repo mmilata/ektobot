fbc39fc1260cb3eaaaf04f94f47e336ee4ce71b1
Martin Milata <mmilata@srck.net>
2013-11-05 01:09:36 +0100

    Add setup.py
    Closes #3.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ektobot',
+      version='0.1',
+      description='Tool for manipulating music albums',
+      long_description = open('README.rst').read(),
+      url='http://github.com/mmilata/ektobot',
+      author='Martin Milata',
+      author_email='b42@srck.net',
+      packages=['ektobot'],
+      scripts=['ektobot.py'],
+      license='WTFPL')
