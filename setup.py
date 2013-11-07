c6e6d3ca51196fe556ac790b986110cad09c0006
Martin Milata <mmilata@srck.net>
2013-11-07 21:37:19 +0100

    Adjust setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ektobot',
       version='0.1',
       description='Tool for manipulating music albums',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 2 :: Only',
+          'Topic :: Communications :: File Sharing',
+          'Topic :: Multimedia :: Sound/Audio',
+      ],
       long_description = open('README.rst').read(),
       url='http://github.com/mmilata/ektobot',
       author='Martin Milata',
       author_email='b42@srck.net',
-      packages=['ektobot'],
+      packages=find_packages(),
       scripts=['bin/ektobot'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
       license='WTFPL')
