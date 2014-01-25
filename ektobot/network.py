mmilata/ektobot

commit c7a27de6147d72624de81623638cba93ad65c13a

youtube: use tags as keywords
Closes #13.

ektobot/network.py
@@ -49,23 +49,19 @@ def process_list(meta, listfile, dry_run, auth=None, keep=False, retry=False):
 
         process_url(meta, url, None, dry_run, auth=auth, keep=keep)
 
-def download_archive(page_url, directory, zip_url=None):
-    logger = logging.getLogger('tags')
-    e = Ektoplazm(page_url)
-    logger.info(u', '.join(e.tags))
-    return e.download_archive(directory)
-
-def process_url(meta, page_url, zip_url=None, dry_run=False, auth=None, keep=False):
+def process_url(meta, page_url, dry_run=False, auth=None, keep=False):
     logger = logging.getLogger('url')
     logger.info(u'Processing {0}'.format(page_url))
 
     try:
         with TemporaryDir('ektobot', keep=keep) as dname:
-            archive = download_archive(page_url, dname)
+            e = Ektoplazm(page_url)
+            logger.info(u'tags: ' + u', '.join(e.tags))
+            archive = e.download_archive(dname)
             mp3_dir = unpack(archive, dry_run=False, outdir=dname)
             video_dir = os.path.join(dname, 'video')
             videos(mp3_dir, dry_run=False, outdir=video_dir, cover=None)
-            ytupload(video_dir, dry_run=dry_run, auth=auth, url=page_url)
+            ytupload(video_dir, dry_run=dry_run, auth=auth, url=page_url, tags=e.tags)
     except KeyboardInterrupt:
         raise
     except:

ektobot/youtube.py
@@ -18,7 +18,8 @@ ektoplazm_description = u'''Download the full album from Ektoplazm: {albumurl}
 Artist: {artist}
 Track: {track}
 Album: {album}
-Track number: {trackno}'''
+Track number: {trackno}
+Tags: {tags}'''
 
 
 default_description = u'''Artist: {artist}
@@ -48,16 +49,20 @@ def ytlogin(email, passwd, dry_run=False):
 
     return yt_service
 
-def ytupload(dirname, dry_run, auth, url=None):
+def ytupload(dirname, dry_run, auth, url=None, tags=set()):
     import gdata.youtube
 
     logger = logging.getLogger('youtube')
 
-    def yt_upload_video(yt_service, filename, title, description):
+    def yt_upload_video(yt_service, filename, title, description, tags):
+        keywords = 'ektoplazm, music'
+        if tags:
+            keywords += (', ' + ', '.join(tags))
+
         media_group = gdata.media.Group(
             title       = gdata.media.Title(text=title),
             description = gdata.media.Description(description_type='plain', text=description),
-            keywords    = gdata.media.Keywords(text='ektoplazm, music'),
+            keywords    = gdata.media.Keywords(text=keywords),
             category    = gdata.media.Category(text='Music', label='Music', scheme='http://gdata.youtube.com/schemas/2007/categories.cat'),
             player      = None
         )
@@ -109,13 +114,14 @@ def ytupload(dirname, dry_run, auth, url=None):
             track = trk['track'],
             album = meta['album'],
             trackno = trk['num'],
+            tags = ', '.join(tags),
             albumurl = url if url else 'http://www.example.org/' #'http://www.ektoplazm.com/'
         )
         logger.info(u'Uploading {0}'.format(title))
         logger.debug(u'Filename {0}'.format(filename))
         logger.debug(u'Description:\n{0}'.format(description))
         if not dry_run:
-            vid_id = yt_upload_video(yt_service, filename, title, description)
+            vid_id = yt_upload_video(yt_service, filename, title, description, tags)
             playlist_ids.append(vid_id)
         logger.debug('Upload complete')
         time.sleep(60) # youtube's not happy when we're uploading too fast

commit 725336346fe538146aa34d0deb10d13c556a85ee

Fix pylint errors
Especially argument mismatch introduced in
c7a27de6147d72624de81623638cba93ad65c13a

ektobot/network.py
@@ -15,19 +15,12 @@ def watch_rss(meta, dry_run, auth=None, keep=False, sleep_interval=30*60):
 
     logger = logging.getLogger('rss')
 
-    def mp3_link(e):
-        for l in e.links:
-            if 'rel' in l and l.rel == 'enclosure' and 'title' in l and l.title == 'MP3 Download':
-                return l.href
-        return None
-
     while True:
         feed = feedparser.parse(meta.feed) # XXX may throw exception
 
         for entry in feed.entries:
             if not meta.is_processed(entry.link):
-                process_url(meta, entry.link, mp3_link(entry), dry_run, auth=auth,
-                            keep=keep)
+                process_url(meta, entry.link, dry_run, auth=auth, keep=keep)
         try:
             time.sleep(sleep_interval)
         except KeyboardInterrupt:
@@ -47,7 +40,7 @@ def process_list(meta, listfile, dry_run, auth=None, keep=False, retry=False):
             else:
                 continue
 
-        process_url(meta, url, None, dry_run, auth=auth, keep=keep)
+        process_url(meta, url, dry_run, auth=auth, keep=keep)
 
 def process_url(meta, page_url, dry_run=False, auth=None, keep=False):
     logger = logging.getLogger('url')

ektobot/source.py
@@ -68,7 +68,7 @@ class Source(object):
         return set()
 
     def get_archive_link(self):
-        raise NotImplemented('archive_link')
+        raise NotImplementedError('archive_link')
 
     def _url_file_name(self, fh):
         try: